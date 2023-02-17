7228b1c5f434a55d2a4fb96be29b522d1daa1a89
TakahiroKz <KizugaTakahiro@gmail.com>
2023-02-16 22:31:58 -0500

    CASA: Mostrando variables y diccionarios

--- my-movie-api/main.py
+++ my-movie-api/main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
+app.title = "Mi aplicacion con FastApi"
+app.version = "0.0.1"
 
+movies=[
+        {
+            "id": 1,
+            "title": "Avatar",
+            "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+            "year": "2009",
+            "rating": 7.8,
+            "category": "Acción"
+        }
+]
 
-
-@app.get('/')
+@app.get('/', tags=['home']) #tags= permite cambiar un titulo de una API
 
 def message():
-    x = 4 + 4 
-    return "Te amo victoria bb uwu"
+    x = "Leonardo Suarez"   
+    return HTMLResponse(f'<h1> Hello world {x}</h1>')
+
+@app.get('/movies', tags=['movies'])
+
+def get_movies():
+    return movies
