a7340242030be40a7cc2a5ba74a80c53fd555c99
TakahiroKz <KizugaTakahiro@gmail.com>
2023-02-18 20:13:08 -0500

    Commit 2: Funcion de filtrar por id

--- my-movie-api/main.py
+++ my-movie-api/main.py
@@ -7,29 +7,36 @@ app.title = "Mi aplicacion con FastApi"
 app.version = "0.0.1"
 
 movies=[
-        {
-            "id": 1,
-            "title": "Avatar",
-            "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-            "year": "2009",
-            "rating": 7.8,
-            "category": "Acción"
-        }
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    },
+    {
+        "id": 2,
+        "title": "InterStellar",
+        "overview": "Un viaje con un buen e infinito fin",
+        "year": "2012",
+        "rating": 9.3,
+        "category": "Drama"
+    }
 ]
 
 @app.get('/', tags=['home']) #tags= permite cambiar un titulo de una API
-
 def message():
     x = "Leonardo Suarez"   
     return HTMLResponse(f'<h1> Hello world {x}</h1>')
 
 @app.get('/movies', tags=['movies'])
-
 def get_movies():
     return movies
 
-@app.get('/movies', tags=['movies'])
-
-def get_movies_by_category(category:str):
-
-    return category
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id:int):
+    for item in movies:
+        if item["id"] == id:
+            return item
+    return []
