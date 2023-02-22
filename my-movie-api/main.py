03bfbe180637426e7d48fbf3cbf041628dd07e10
TakahiroKz <KizugaTakahiro@gmail.com>
2023-02-21 21:42:42 -0500

    Commit 7: Actualizacion General

--- my-movie-api/main.py
+++ my-movie-api/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Body, Path, Query
+from fastapi import FastAPI, Body, Path, Query, Request, HTTPException, Depends
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from jwt_manager import create_token, validate_token
+from fastapi.security import HTTPBearer
 
 
 app = FastAPI()
@@ -9,6 +11,16 @@ app.title = "Mi aplicacion con FastApi"
 app.version = "0.0.1"
 
 #Esquema para recibir datos.
+
+class JWTBearer(HTTPBearer):
+    async def __call__(self, request:Request):
+        auth = await super().__call__(request)
+        data = validate_token(auth.credentials)
+        if data['email'] != "admin@gmail.com":
+            raise HTTPException(status_code=403, detail="Credenciales son invalidas")
+
+
+
 class Movie(BaseModel):
     id: Optional[int] = None
     title : str = Field(max_length=15)
@@ -28,6 +40,10 @@ class Movie(BaseModel):
                 "category" : "Acción"
             }
         }
+
+class user(BaseModel):
+    email:str
+    password:str
 
 movies=[
     {
@@ -53,12 +69,22 @@ def message():
     x = "Leonardo Suarez"   
     return HTMLResponse(f'<h1> Hello world {x}</h1>')
 
-#Retornar todas la pelicular
-@app.get('/movies', tags=['movies'],response_model=List[Movie], status_code=200) 
+#Autenticacion
+@app.post('/login', tags=['auth'])
+def login(user: user):
+    if user.email =="admin@gmail.com" and user.password == "admin":
+        token:str = create_token(user.dict())
+        return JSONResponse(status_code=200,content=token)
+    else:
+        return JSONResponse(content="Usuario incorrecto")
+
+
+#Retornar todas la peliculas
+@app.get('/movies', tags=['movies'],response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())]) 
 def get_movies() -> List[Movie]:
     return JSONResponse(status_code=200,content=movies)
 
-#Retornar pelicula filtrada por ID, Validaciones parametros ruta
+#Retornar pelicula filtrada por ID, Validaciones parametros ruta / Path
 @app.get('/movies/{id}', tags=['movies'],response_model=Movie,status_code=200)
 def get_movie(id:int = Path(ge=1, le=2000))-> Movie :
     for item in movies:
