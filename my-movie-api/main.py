76e59153891e198879a7cdcd02c922f0790572f1
TakahiroKz <KizugaTakahiro@gmail.com>
2023-03-02 21:43:25 -0500

    Se realiza la insercion a base de datos

--- my-movie-api/main.py
+++ my-movie-api/main.py
@@ -4,8 +4,9 @@ from pydantic import BaseModel, Field
 from typing import Optional, List
 from jwt_manager import create_token, validate_token
 from fastapi.security import HTTPBearer
-from config.database import session,engine, Base
-from models.movie import Movie
+from models.movie import Movie as MovieModel
+from config.database import Base, session, engine
+
 
 app = FastAPI()
 app.title = "Mi aplicacion con FastApi"
@@ -14,7 +15,6 @@ app.version = "0.0.1"
 Base.metadata.create_all(bind=engine)
 
 #Esquema para recibir datos.
-
 
 class JWTBearer(HTTPBearer):
     async def __call__(self, request:Request):
@@ -27,7 +27,7 @@ class Movie(BaseModel):
     id: Optional[int] = None
     title : str = Field(max_length=15)
     overview : str = Field(min_length=5,max_length=50)
-    year : str = Field(max_length="4")
+    year : int = Field(le=3000)
     rating : float = Field(ge=1, le=10)
     category : str = Field(min_length=5, max_length=20)
 
@@ -37,7 +37,7 @@ class Movie(BaseModel):
                 "id" : 1,
                 "title": "Mi pelicula",
                 "overview" : "Descripcion de la pelicula",
-                "year": "2023",
+                "year": 2023,
                 "rating" : 10.0,
                 "category" : "Acción"
             }
@@ -82,8 +82,10 @@ def login(user: user):
 
 
 #Retornar todas la peliculas
-@app.get('/movies', tags=['movies'],response_model=List[Movie], status_code=200,dependencies=[Depends(JWTBearer())])
+@app.get('/movies', tags=['movies'],response_model=List[Movie], status_code=200) #,dependencies=[Depends(JWTBearer())]
 def get_movies() -> List[Movie]:
+    db = session()
+    result = db.query().all()
     return JSONResponse(status_code=200,content=movies)
 
 #Retornar pelicula filtrada por ID, Validaciones parametros ruta / Path
@@ -99,13 +101,18 @@ def get_movie(id:int = Path(ge=1, le=2000))-> Movie :
 #Obtener peliculas filtradas por categoria Validacion parametros Query
 @app.get('/movies/',tags=['movies'],response_model=List[Movie],status_code=200)
 def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]:
-    data = [item for item in movies  if item["category"] == category]
+    data = [item for item in movies  if item["category"] == category.capitalize()]
     return JSONResponse(status_code=200, content=data)
 
 #Crear una nueva pelicula
 @app.post('/movies',tags=['movies'],response_model=dict,status_code=201)
 def create_movie(movie: Movie) -> dict:
-    movies.append(movie)
+    db = session()
+    print(movie)
+    new_movie = MovieModel(**movie.dict())
+    print("Nueva pelicula insertada: ",new_movie)
+    db.add(new_movie)
+    db.commit()
     return JSONResponse(status_code=201,content={"message":"Se ha registrado la pelicula"})
 
 #Modificar una pelicula
--- my-movie-api/models/movie.py
+++ my-movie-api/models/movie.py
@@ -1,15 +1,11 @@
 from config.database import Base
-from sqlalchemy import Column, Integer,String,Float
+from sqlalchemy import Column, Integer, String, Float
 
 class Movie(Base):
-
-    __tablename__ = "movies"
-
+    __tablename__ = "movie" #se usa para indicar que se debe crear una tabla
     id = Column(Integer, primary_key = True)
     title = Column(String)
     overview = Column(String)
-    year = Column(String)
+    year = Column(Integer)
     rating = Column(Float)
     category = Column(String)
-
-
