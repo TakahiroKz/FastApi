e09745c61e8b69e98503432430b78307e203db29
TakahiroKz <KizugaTakahiro@gmail.com>
2023-03-22 18:40:41 -0500

    Se agregan servicios para consulta de peliculas, peliculas por ID y peliculas por Categoria

--- my-movie-api/routers/movie.py
+++ my-movie-api/routers/movie.py
@@ -9,6 +9,7 @@ from models.movie import Movie as MovieModel
 from fastapi.encoders import jsonable_encoder
 from middleware.error_Handler import ErrorHandler
 from middleware.jwt_bearer import JWTBearer
+from services.movie import MovieService
 
 movie_router = APIRouter()
 
@@ -38,14 +39,14 @@ class Movie(BaseModel):
 @movie_router.get('/movies', tags=['movies'],response_model=List[Movie], status_code=200,dependencies=[Depends(JWTBearer())]) #
 def get_movies() -> List[Movie]:
     db = session()
-    result = db.query(MovieModel).all()
+    result = MovieService(db).get_movies()
     return JSONResponse(status_code=200,content=jsonable_encoder(result))
 
 #Retornar pelicula filtrada por ID, Validaciones parametros ruta / Path
 @movie_router.get('/movies/{id}', tags=['movies'],response_model=Movie,status_code=200)
 def get_movie(id:int = Path(ge=1, le=2000))-> Movie :
     db = session()
-    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    result = MovieService(db).get_movie(id)
     if not result:
         return JSONResponse(status_code=200, content={"message":"No se ha encontrado la pelicula"})
     return JSONResponse(status_code=200,content=jsonable_encoder(result))
@@ -54,7 +55,7 @@ def get_movie(id:int = Path(ge=1, le=2000))-> Movie :
 @movie_router.get('/movies/',tags=['movies'],response_model=List[Movie],status_code=200)
 def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]:
     db = session()
-    result = db.query(MovieModel).filter(MovieModel.category == category.capitalize()).first()
+    result = MovieService(db).get_movie_by_category(category.capitalize())
     if not result:
         return JSONResponse(status_code=200, content={"message":"No se ha encontrado la pelicula"})
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
